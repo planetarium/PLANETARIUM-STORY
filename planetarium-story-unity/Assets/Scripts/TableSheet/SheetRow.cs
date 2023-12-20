e0969504c3f967dced4d569de7ca2475f83dc09f
tyrosine1153 <tyrosine1153@gmail.com>
2023-12-20 15:27:14 +0900

    add TableSheets import script

---
+++ planetarium-story-unity/Assets/Scripts/TableSheet/SheetRow.cs
@@ -0,0 +1,47 @@
+﻿namespace TableSheet
+{
+    public abstract class SheetRow<T>
+    {
+        public abstract T Key { get; }
+        
+        public abstract void Set(string[] fields);
+
+        #region Equals
+
+        private bool Equals(SheetRow<T> other)
+        {
+            return Key.Equals(other.Key);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            
+            if (obj.GetType() != GetType()) return false;
+            return Equals((SheetRow<T>) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Key.GetHashCode();
+        }
+
+        #endregion
+        
+        protected static int ParseInt(string value)
+        {
+            return int.Parse(value);
+        }
+        
+        protected static float ParseFloat(string value)
+        {
+            return float.Parse(value);
+        }
+        
+        protected static TEnum ParseEnum<TEnum>(string value)
+        {
+            return (TEnum)System.Enum.Parse(typeof(TEnum), value);
+        }
+    }
+}
